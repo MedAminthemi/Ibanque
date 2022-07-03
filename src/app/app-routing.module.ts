7741e3774e64fd7dc01704452a64c47cdd4d9206
Mohamed Amine Themi <MAThemi@se.linedata.com>
2022-07-03 14:22:08 +0100

    inital commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DemandeComponent } from './demande/demande.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {component:DemandeComponent , path:"demande"}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'frontibanque';
+  routes = [{name:"demande",path:'demande'}];
 }
