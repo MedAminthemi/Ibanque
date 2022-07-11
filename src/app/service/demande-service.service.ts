47489d6d1c208014a6d29762234f62ab43f42af7
Mohamed Amine Themi <MAThemi@se.linedata.com>
2022-07-11 23:04:47 +0100

    valider refuser demande

--- src/app/service/demande-service.service.ts
+++ src/app/service/demande-service.service.ts
@@ -1,6 +1,13 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-
+import { Observable } from 'rxjs';
+class Demande {
+  reference : any;
+  dateCeation :any;
+  libelle:any;
+  statut:any;
+  user:any;
+}
 @Injectable({
   providedIn: 'root'
 })
@@ -19,5 +26,14 @@ export class DemandeServiceService {
 
   getAllChequier(){
     return this.http.get('http://localhost:8080/api/v1/alldemandecheq');
+  }
+  saveDemandeCC(a:Demande){
+    return this.http.post('http://localhost:8080/api/v1/demandeCreationCPT',a);
+  }
+  saveDemandeC(a:any){
+   return this.http.post('http://localhost:8080/api/v1/demandeCredit',a) ;
+  }
+  saveDemandeCh(a:Demande){
+    return this.http.post('http://localhost:8080/api/v1/demandecheq',a);
   }
 }
