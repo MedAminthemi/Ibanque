5fcf0a7265ccfc44904bcb499ed5e4064906c1ca
Mohamed Amine Themi <MAThemi@se.linedata.com>
2022-07-21 22:03:33 +0100

    enhacments

--- src/app/demande-list/demande-list.component.ts
+++ src/app/demande-list/demande-list.component.ts
@@ -1,4 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { DemandeService } from 'app/service/demande.service';
+class Demande {
+  reference : any;
+  dateCeation :any;
+  libelle:any;
+  statut:any;
+  user:any;
+}
 
 @Component({
   selector: 'app-demande-list',
@@ -6,10 +14,73 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./demande-list.component.css']
 })
 export class DemandeListComponent implements OnInit {
-
-  constructor() { }
-
+  constructor(private demandeService : DemandeService) { }
+  demandesCredits : any = [];
+  demandesCreationCompte : any = [];
+  demandeChequier : any= [];
+  demande : Demande ;
+  
   ngOnInit(): void {
-  }
+   
+    
+    this.demandeService.getAlldemandesCredit().subscribe((data) => {
+        console.log(data);
+        this.demandesCredits = data;
+    
+      }); 
+
+      this.demandeService.getAlldemandesCreationCompte().subscribe((data) => {
+        this.demandesCreationCompte = data;
+      });
+      this.demandeService.getAllChequier().subscribe((data) => {
+        this.demandeChequier = data;
+      });
+
+}
+
+ValiderDemandeCC(a:Demande){
+  a.statut = 'V'
+  this.demandeService.saveDemandeCC(a).subscribe((data) => {
+    console.log(data);
+  });
+}
+
+
+ValiderDemandeC(a:any){
+
+  a.statut="V";
+  this.demandeService.saveDemandeC(a).subscribe((data) => {
+    console.log(data);
+  });
+
+
+}
+ValiderDemandeCh(a:Demande){
+  a.statut = 'V'
+  this.demandeService.saveDemandeCh(a).subscribe((data) => {
+    console.log(data);
+  });
+}
+
+refuserDemandeC(a:Demande){
+  a.statut = 'R'
+  this.demandeService.saveDemandeC(a).subscribe((data) => {
+    console.log(data);
+  });
+  
+}
+
+refuserDemandeCC(a:Demande){
+  a.statut = 'R'
+  this.demandeService.saveDemandeCC(a).subscribe((data) => {
+    console.log(data);
+  });
+}
+refuserDemandeCh(a:Demande){
+  a.statut = 'R'
+  this.demandeService.saveDemandeCh(a).subscribe((data) => {
+    console.log(data);
+  });
+}
 
 }
